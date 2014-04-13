97aae2440c948e5ccb0049841fef2f33cd814edd
andrea <andreadelucia@hotmail.com>
2014-04-13 16:43:47 +0200

    Game

---
+++ Deep_Space_Explorer/Solar_System/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Solar_System
+//
+//  Created by Fefy on 12/04/14.
+//  Copyright (c) 2014 Fefy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
